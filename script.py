f7d1755f9266677b5d7ac9491a0843e9fc14721b
Maria Sharabayko <msharabayko@haivision.com>
2020-02-20 18:01:52 +0100

    Joined rcv and snd stats, first version

--- script.py
+++ script.py
@@ -58,21 +58,65 @@ def main():
 
     print('Sender statisitcs')
     print(snd_stats.head(10))
+    print(snd_stats.tail(10))
     print('Receiver statistics')
     print(rcv_stats.head(10))
+    print(rcv_stats.tail(10))
 
     # TODO: Adjust clocks
 
     # Combine sender and receiver data sets using linear interpolation.
     # TODO: Experiment with different interpolation methods for different features.
+    # rcv_stats['isSender'] = False
     snd_stats = snd_stats.add_suffix('_snd')
     rcv_stats = rcv_stats.add_suffix('_rcv')
+    snd_stats['isSender'] = True
     stats = snd_stats.join(rcv_stats, how='outer')
-    # stats = snd_stats.join(rcv_stats, how='outer', lsuffix='_snd', rsuffix='_rcv')
-    # stats = stats.interpolate()
-    # stats = stats.dropna()
-    print(stats.head(20))
+    # Cut dataframe by sender time
+    # TODO: cut the time properly depending on different situations
+    start_timestamp = snd_stats.index[0]
+    end_timestamp = snd_stats.index[-1]
+    stats = stats[(stats.index >= start_timestamp) & (stats.index <= end_timestamp)]
+    # TODO: Fill NA values for the start point properly
+    # stats.loc[start_timestamp, 'pktRecv_rcv'] = 0
 
+    stats['isSender'] = stats['isSender'].fillna(False)
+    stats['timeDiff'] = stats.index.to_series().diff().fillna(pd.to_timedelta(0))
+    stats['timeDiffShifted'] = stats['timeDiff'].shift().fillna(pd.to_timedelta(0))
+    
+    print('Combined stats')
+    print(stats.info())
+    print(stats.head(20))
+    print(stats.tail(20))
+
+    df = stats[stats['isSender'] == False]
+    df = df[['isSender', 'pktRecv_rcv', 'timeDiffShifted', 'timeDiff']]
+    df = df.rename(columns={'timeDiffShifted': 'timeDiff_p1', 'timeDiff': 'timeDiff_p2'})
+
+    df['timeDiff'] = df.index.to_series().diff()
+    df['timeDiff'] = df['timeDiff'].fillna(df['timeDiff'].mean())
+    
+    df['timeDiff_p1'] = df['timeDiff_p1'] / df['timeDiff']
+    df['timeDiff_p2'] = df['timeDiff_p2'] / df['timeDiff']
+
+    df['pktRecv_rcv_p1'] = (df['timeDiff_p1'] * df['pktRecv_rcv']).round().astype('int32')
+    df['pktRecv_rcv_p2'] = (df['timeDiff_p2'] * df['pktRecv_rcv']).round().astype('int32')
+
+    df['pktRecv_rcv_new'] = df['pktRecv_rcv_p1'] + df['pktRecv_rcv_p2'].shift().fillna(0)
+
+    print(df.head(20))
+    print(df.tail(20))
+    
+    result = snd_stats.join(df['pktRecv_rcv_new'], how='outer')
+    result['pktRecv_rcv_new'] = result['pktRecv_rcv_new'].shift(-1)
+
+    print('Result')
+    print(result.head(20))
+
+    result['isSender'] = result['isSender'].fillna(False)
+    result = result[result['isSender']]
+    print(result.head(20))
+    
     # SRT statistics vs Thark dump
     # Extract SRT packets from .pcapng tshark dump file:
 
